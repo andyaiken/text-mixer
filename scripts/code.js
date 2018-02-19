d3edea6b5592ad9ef0d7bfd77898ec53e7d00871
Andy Aiken <aaiken@scottlogic.com>
2018-02-19 20:56:52 +0000

    Markov mechanics

--- scripts/code.js
+++ scripts/code.js
@@ -24,7 +24,7 @@ function buildModel(sources) {
         sources.forEach(source => {
             var lines = source.split(/\r?\n/);
             lines.forEach(line => {
-                // TODO: Add this line to the model
+                addLineToModel(line, model);
             });
         });
     }
@@ -32,7 +32,75 @@ function buildModel(sources) {
     return model;
 }
 
+function addLineToModel(line, model) {
+    line = String.fromCharCode(0, 1) + line + String.fromCharCode(2);
+
+    for (var index = 2; index != line.length; ++index) {
+        var prev = line.substr(index - 2, 2);
+        var char = line.substr(index, 1);
+
+        var item = null;
+        model.forEach(x => {
+            if (x.prev === prev) {
+                item = x;
+            }
+        });
+        if (!item) {
+            item = {
+                prev: prev,
+                freq: []
+            };
+            model.push(item);
+        }
+
+        var freq = null;
+        item.freq.forEach(x => {
+            if (x.char === char) {
+                freq = x;
+            }
+        });
+        if (!freq) {
+            freq = {
+                char: char,
+                count: 0
+            };
+            item.freq.push(freq);
+        }
+
+        freq.count += 1;
+    }
+}
+
 function extract(model) {
-    // TODO: Extract a line from the model
-    return "xxx";
+    var line = String.fromCharCode(0, 1);
+
+    while (true) {
+        var prev = line.substr(line.length - 2, 2);
+
+        var item = null;
+        model.forEach(x => {
+            if (x.prev === prev) {
+                item = x;
+            }
+        });
+        if (item) {
+            var candidates = "";
+            item.freq.forEach(freq => {
+                candidates += freq.char.repeat(freq.count);
+            });
+            var index = Math.floor(Math.random() * candidates.length);
+            var char = candidates[index];
+
+            if (char === String.fromCharCode(2)) {
+                line = line.substr(2);
+                return line;
+            } else {
+                line += char;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    return null;
 }
